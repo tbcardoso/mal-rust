57c29ef76c2a6160f1085b8281347f5006dc3da7
Tiago Cardoso <tbcardoso@outlook.com>
2018-10-03 22:55:30 +0100

    Add boilerplate for step2

---
+++ rust/src/bin/step1_read_print.rs
@@ -0,0 +1,36 @@
+extern crate malrs;
+
+use malrs::readline::Readline;
+
+fn main() {
+    let mut readline = Readline::new();
+
+    loop {
+        match readline.readline() {
+            None => break,
+            Some(line) => {
+                if !line.is_empty() {
+                    println!("{}", rep(line.to_string()));
+                }
+            }
+        }
+    }
+
+    readline.save_history();
+}
+
+fn rep(s: String) -> String {
+    print(eval(read(s)))
+}
+
+fn read(s: String) -> String {
+    s
+}
+
+fn eval(s: String) -> String {
+    s
+}
+
+fn print(s: String) -> String {
+    s
+}
