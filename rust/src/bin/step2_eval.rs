379210ee3b27ad85059b938b95be1bdfc6aeec90
Tiago Cardoso <tbcardoso@outlook.com>
2018-10-14 19:44:39 +0100

    Add boilerplate for step 2

---
+++ rust/src/bin/step2_eval.rs
@@ -0,0 +1,45 @@
+extern crate malrs;
+
+use malrs::printer::pr_str;
+use malrs::reader::read_str;
+use malrs::readline::Readline;
+use malrs::types::{MalError, MalResult, MalValue};
+
+fn main() {
+    let mut readline = Readline::new();
+
+    loop {
+        match readline.readline() {
+            None => break,
+            Some(line) => {
+                if !line.is_empty() {
+                    match rep(&line) {
+                        Ok(result) => println!("{}", result),
+                        Err(MalError::EmptyProgram) => {}
+                        Err(mal_error) => println!("Error! {}", mal_error),
+                    }
+                }
+            }
+        }
+    }
+
+    readline.save_history();
+}
+
+fn rep(s: &str) -> Result<String, MalError> {
+    let read_val = read(s)?;
+    let eval_val = eval(&read_val)?;
+    Ok(print(&eval_val))
+}
+
+fn read(s: &str) -> MalResult {
+    read_str(s)
+}
+
+fn eval(mal_val: &MalValue) -> Result<&MalValue, MalError> {
+    Ok(mal_val)
+}
+
+fn print(mal_val: &MalValue) -> String {
+    pr_str(mal_val)
+}
