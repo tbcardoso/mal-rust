a965de6fb74ca439438480bc30dd59137d837536
Tiago Cardoso <tbcardoso@outlook.com>
2018-12-08 21:06:00 +0000

    Add tests to env module

--- rust/src/env.rs
+++ rust/src/env.rs
@@ -24,3 +24,30 @@ impl Env {
             .ok_or_else(|| MalError::UndefinedSymbol(symbol_key.to_string()))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use env::Env;
+    use types::MalValueType::Str;
+
+    #[test]
+    fn test_get_from_empty_env() {
+        let env = Env::new();
+
+        assert_eq!(
+            env.get("symbol"),
+            Err(MalError::UndefinedSymbol("symbol".to_string()))
+        );
+    }
+
+    #[test]
+    fn test_set_and_get() {
+        let mut env = Env::new();
+        let val = MalValue::new(Str("abc".to_string()));
+
+        env.set("sym", val.clone());
+
+        assert_eq!(env.get("sym"), Ok(val));
+    }
+}
