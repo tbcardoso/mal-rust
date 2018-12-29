37af10c2872b7897c5ed4f2cd7f130fada3bcabd
Tiago Cardoso <tbcardoso@outlook.com>
2018-12-29 17:34:32 +0000

    Implement special form 'let*'

--- rust/src/bin/step3_env.rs
+++ rust/src/bin/step3_env.rs
@@ -6,7 +6,7 @@ use malrs::types::MalValueType::{List, Number, RustFunc, Symbol};
 use malrs::types::{MalError, MalResult, MalValue, RustFunction};
 
 fn main() {
-    let mut env = create_env();
+    let mut env = create_root_env();
     let mut readline = Readline::new();
 
     loop {
@@ -27,7 +27,7 @@ fn main() {
     readline.save_history();
 }
 
-fn create_env() -> Env {
+fn create_root_env() -> Env {
     let mut env = Env::new();
 
     env.set(
@@ -106,6 +106,7 @@ fn eval(ast: &MalValue, env: &mut Env) -> MalResult {
 
             match *first_arg.mal_type {
                 Symbol(ref name) if name == "def!" => apply_special_form_def(&list[1..], env),
+                Symbol(ref name) if name == "let*" => apply_special_form_let(&list[1..], env),
                 _ => apply_ast(ast, env),
             }
         }
@@ -165,13 +166,56 @@ fn apply_special_form_def(args: &[MalValue], env: &mut Env) -> MalResult {
         Ok(symbol)
     } else {
         Err(MalError::SpecialForm(
-            "First argument must be a valid symbol name".to_string(),
+            "def! first argument must be a valid symbol name".to_string(),
         ))
     }?;
 
     let arg2 = eval(&args[1], env)?;
 
     env.set(arg1.as_str(), arg2.clone());
+
+    Ok(arg2)
+}
+
+fn apply_special_form_let(args: &[MalValue], env: &Env) -> MalResult {
+    if args.len() != 2 {
+        return Err(MalError::SpecialForm(format!(
+            "let* expected 2 arguments, got {}",
+            args.len()
+        )));
+    }
+
+    let bindings = if let List(ref bindings) = *args[0].mal_type {
+        Ok(bindings.as_slice())
+    } else {
+        Err(MalError::SpecialForm(
+            "let* first argument must be a list".to_string(),
+        ))
+    }?;
+
+    if bindings.len() % 2 != 0 {
+        return Err(MalError::SpecialForm(
+            "let* bindings list must have an even number of elements".to_string(),
+        ));
+    }
+
+    let mut inner_env = Env::with_outer_env(env);
+
+    for i in (0..bindings.len() - 1).step_by(2) {
+        let binding_name = if let Symbol(ref symbol) = *bindings[i].mal_type {
+            Ok(symbol)
+        } else {
+            Err(MalError::SpecialForm(
+                "let* odd numbered elements of binding list must be valid symbol names".to_string(),
+            ))
+        }?;
+
+        let binding_expr = eval(&bindings[i + 1], &mut inner_env)?;
+
+        inner_env.set(binding_name.as_str(), binding_expr);
+    }
+
+    let arg2 = eval(&args[1], &mut inner_env)?;
 
     Ok(arg2)
 }
@@ -183,25 +227,25 @@ mod tests {
 
     #[test]
     fn test_empty_program() {
-        let mut env = create_env();
+        let mut env = create_root_env();
         assert_eq!(rep("", &mut env), Err(EmptyProgram));
     }
 
     #[test]
     fn test_empty_list() {
-        let mut env = create_env();
+        let mut env = create_root_env();
         assert_eq!(rep("()", &mut env), Ok("()".to_string()));
     }
 
     #[test]
     fn test_nested_arithmetic() {
-        let mut env = create_env();
+        let mut env = create_root_env();
         assert_eq!(rep("(+ 2 (* 3 4))", &mut env), Ok("14".to_string()));
     }
 
     #[test]
     fn test_special_form_def() {
-        let mut env = create_env();
+        let mut env = create_root_env();
         assert_eq!(
             rep("(def! str1 \"abc\")", &mut env),
             Ok("\"abc\"".to_string())
@@ -211,17 +255,29 @@ mod tests {
 
     #[test]
     fn test_special_form_def_evaluates_2nd_par() {
-        let mut env = create_env();
+        let mut env = create_root_env();
         assert_eq!(rep("(def! x (- 5 3))", &mut env), Ok("2".to_string()));
         assert_eq!(rep("x", &mut env), Ok("2".to_string()));
     }
 
     #[test]
     fn test_special_form_def_symbol_to_symbol() {
-        let mut env = create_env();
+        let mut env = create_root_env();
         assert_eq!(rep("(def! x 1)", &mut env), Ok("1".to_string()));
         assert_eq!(rep("(def! y x)", &mut env), Ok("1".to_string()));
         assert_eq!(rep("x", &mut env), Ok("1".to_string()));
         assert_eq!(rep("y", &mut env), Ok("1".to_string()));
+    }
+
+    #[test]
+    fn test_special_form_let() {
+        let mut env = create_root_env();
+        assert_eq!(rep("(let* (c 2) (+ 3 c))", &mut env), Ok("5".to_string()));
+    }
+
+    #[test]
+    fn test_special_form_let_multiple_bindings() {
+        let mut env = create_root_env();
+        assert_eq!(rep("(let* (a 2 b (+ a a) c (- b a)) (+ (* a b) c))", &mut env), Ok("10".to_string()));
     }
 }
