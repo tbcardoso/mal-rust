ce3d8439f21d20161d76b558979131b802a09fc9
Tiago Cardoso <tbcardoso@outlook.com>
2019-02-24 16:05:18 +0000

    Refactor core module

--- rust/src/core.rs
+++ rust/src/core.rs
@@ -1,33 +1,34 @@
+use crate::env::Env;
 use crate::types::MalValueType::{Number, RustFunc};
 use crate::types::{MalError, MalResult, MalValue, RustFunction};
 
 pub fn ns() -> Vec<(&'static str, MalValue)> {
     vec![
-        (
-            "+",
-            MalValue::new(RustFunc(RustFunction(|args, _| {
-                eval_arithmetic_operation(args, |a, b| a + b)
-            }))),
-        ),
-        (
-            "-",
-            MalValue::new(RustFunc(RustFunction(|args, _| {
-                eval_arithmetic_operation(args, |a, b| a - b)
-            }))),
-        ),
-        (
-            "*",
-            MalValue::new(RustFunc(RustFunction(|args, _| {
-                eval_arithmetic_operation(args, |a, b| a * b)
-            }))),
-        ),
-        (
-            "/",
-            MalValue::new(RustFunc(RustFunction(|args, _| {
-                eval_arithmetic_operation(args, |a, b| a / b)
-            }))),
-        ),
+        ("+", rust_func(add)),
+        ("-", rust_func(subtract)),
+        ("*", rust_func(multiply)),
+        ("/", rust_func(divide)),
     ]
+}
+
+fn rust_func(func: fn(&[MalValue], &mut Env) -> MalResult) -> MalValue {
+    MalValue::new(RustFunc(RustFunction(func)))
+}
+
+fn add(args: &[MalValue], _env: &mut Env) -> MalResult {
+    eval_arithmetic_operation(args, |a, b| a + b)
+}
+
+fn subtract(args: &[MalValue], _env: &mut Env) -> MalResult {
+    eval_arithmetic_operation(args, |a, b| a - b)
+}
+
+fn multiply(args: &[MalValue], _env: &mut Env) -> MalResult {
+    eval_arithmetic_operation(args, |a, b| a * b)
+}
+
+fn divide(args: &[MalValue], _env: &mut Env) -> MalResult {
+    eval_arithmetic_operation(args, |a, b| a / b)
 }
 
 fn eval_arithmetic_operation(args: &[MalValue], op: fn(f64, f64) -> f64) -> MalResult {
